9f1ac04221f2850788b2b7f23e55be505febe013
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2019-12-24 16:56:39 -0500

    Fix quotes

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
-import path from "path";
-import util from "util";
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
 
 export class FileServiceFS implements FileService {
   private basePath: string;
