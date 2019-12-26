36cb45c91f09e50ffd7dfe67020bc84a10a7f892
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2019-12-26 17:30:26 -0500

    Enforce @Route function type

--- src/routes/core/route.ts
+++ src/routes/core/route.ts
@@ -1,11 +1,13 @@
 import { SocketController } from './controller';
 
+export type SocketRouteFunction = (m: any) => Promise<void>;
+
 export type SocketRouteOptions = {
   auth?: boolean;
 }
 
 export function Route(subject?: string, options?: SocketRouteOptions) {
-  return (target: SocketController, propertyKey: string, descriptor: PropertyDescriptor) => {
+  return (target: SocketController, propertyKey: string, descriptor: TypedPropertyDescriptor<SocketRouteFunction>) => {
     target.addRoute(propertyKey, subject, options);
   }
 }
